c551d6019aa05b11508bc476cfe73868f9726633
Baris Ergun <barisergun75@gmail.com>
2017-08-12 13:47:36 +0100

    Feature added to be able support external schema as StructType

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -13,3 +13,5 @@ addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
 addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.9")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.3")
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
---
+++ src/main/scala/com/crealytics/spark/excel/schema/DefaultSource.scala
@@ -0,0 +1,24 @@
+package com.crealytics.spark.excel.schema
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.sources.SchemaRelationProvider
+import org.apache.spark.sql.types.StructType
+import com.crealytics.spark.excel.ExcelRelation
+import com.crealytics.spark.excel.ParameterExtractor
+
+class DefaultSource 
+  extends SchemaRelationProvider 
+  with ParameterExtractor{
+
+  /**
+    * Creates a new relation for retrieving data from an Excel file
+    */
+  override def createRelation(
+                               sqlContext: SQLContext,
+                               parameters: Map[String, String],
+                               schema: StructType): ExcelRelation = {
+    implicit def context = sqlContext
+    implicit def schemaOpt: Option[StructType] = Some(schema)
+    ExcelRelation.createExcelRelation(parameters)
+  }  
+}
