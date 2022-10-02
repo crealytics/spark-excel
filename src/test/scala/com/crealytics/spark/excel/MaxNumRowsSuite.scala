16d71c39a42e6cc21e17bac57bdafe9bb9bd9ad4
PJ Fanning <pjfanning@users.noreply.github.com>
2022-10-02 14:18:53 +0100

    v1 streaming read test

---
+++ src/test/scala/com/crealytics/spark/excel/MaxNumRowsSuite.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Martin Mauch (@nightscape)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crealytics.spark.excel
+
+import breeze.linalg.max
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class MaxNumRowsSuite extends AnyWordSpec with DataFrameSuiteBase with Matchers {
+
+  "excel v2 and maxNumRows" can {
+
+    s"read with maxNumRows=200" in {
+
+      val dfExcel = spark.read
+        .format("com.crealytics.spark.excel")
+        .option("path", "src/test/resources/v2readwritetest/large_excel/largefile-wide-single-sheet.xlsx")
+        .option("header", value = false)
+        // .option("dataAddress", "'Sheet1'!B7:M16")
+        .option("maxRowsInMemory", "200")
+        .option("inferSchema", false)
+        .load()
+
+      dfExcel.count() shouldEqual 2241
+    }
+
+  }
+}
